3ab9a70e26e2ea0597199f3a0a95c9a19819a097
krahul14 <krahul52002@gmail.com>
2024-10-10 23:05:16 +0530

    format

--- mycontacts-backend/controllers/contactControllers.js
+++ mycontacts-backend/controllers/contactControllers.js
@@ -1,9 +1,9 @@
 const asyncHandler = require("express-async-handler")
 const Contact = require("../models/contactModel")
+
 // @desc Get all contacts
 // @route GET /api/contacts
 // @access Public
-
 const getContacts = asyncHandler(async (req, res) => {
   const contacts = await Contact.find({})
   res.status(200).json(contacts)
@@ -12,19 +12,18 @@ const getContacts = asyncHandler(async (req, res) => {
 // @desc Get contact by id
 // @route GET /api/contacts:id
 // @access Public
-
 const getContactById = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id)
-  if(!contact) {
+  if (!contact) {
     res.status(404)
     throw new Error("Contact not found")
   }
   res.status(200).json(contact)
 })
+
 // @desc create new contacts
 // @route POST /api/contacts
 // @access Public
-
 const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is: ", req.body)
   const { name, email, phone } = req.body
@@ -39,10 +38,9 @@ const createContact = asyncHandler(async (req, res) => {
 // @desc update a contact
 // @route PUT /api/contacts:id
 // @access Public
-
 const updateContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id)
-  if(!contact) {
+  if (!contact) {
     res.status(404)
     throw new Error("Contact not found")
   }
@@ -59,10 +57,9 @@ const updateContact = asyncHandler(async (req, res) => {
 // @desc delete contact
 // @route DELETE /api/contacts:id
 // @access Public
-
 const deleteContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id)
-  if(!contact) {
+  if (!contact) {
     res.status(404)
     throw new Error("Contact not found")
   }
